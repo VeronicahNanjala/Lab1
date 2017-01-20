05d10ad7e052d1bc2cc9e51dfc65b9ef7cad71db
veronicah.nanjala <veronicananjala1@gmail.com>
2017-01-20 20:57:19 +0000

    just finished add

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator{
+  public static void main(String [] param){
+   String add,subtract,multiply,devide;
+    int variable1=12, variable2=50;
+    
+    int sum=variable1+variable2;
+    System.out.println("We are adding 12 and 50");
+    System.out.println("The of 12 and 50 is: " + sum+"\n");
+    
+  }
+}
