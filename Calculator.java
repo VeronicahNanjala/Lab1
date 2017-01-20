0078a4f45bde8d935d7b241cbbd695a2de2f0792
veronicah.nanjala <veronicananjala1@gmail.com>
2017-01-20 21:07:44 +0000

    subtract

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,11 @@ public class Calculator{
     
     int sum=variable1+variable2;
     System.out.println("We are adding 12 and 50");
-    System.out.println("The of 12 and 50 is: " + sum+"\n");
+    System.out.println("The sum of 12 and 50 is: " + sum+"\n");
+    
+    int s=variable2-variable1;
+    System.out.println("We are adding 12 from 50");
+    System.out.println("The difference between 12 and 50 is: " + s+"\n");
     
   }
 }
